43bdccf4d594bcc1e3b8a7e2cb6ab826deace7f7
LucasKakushi <lucasbkakushi@gmail.com>
2026-09-21 17:26:26 -0300

    wip: inicia validacao dos digitos verificadores do CNPJ

--- src/AssistenciaTecnica.Domain/Entities/Cliente.cs
+++ src/AssistenciaTecnica.Domain/Entities/Cliente.cs
@@ -30,12 +30,21 @@ namespace AssistenciaTecnica.Domain.Entities
             ValidarCampoObrigatorio(razaoSocial, nameof(razaoSocial));
             ValidarCampoObrigatorio(cnpj, nameof(cnpj));
 
-            ValidarTentantIdVazio(tenantId, nameof(tenantId));
+            ValidarTenantIdVazio(tenantId, nameof(tenantId));
+
+            var cnpjNormalizado = NormalizarCnpj(cnpj);
+            ValidarCampoObrigatorio(cnpjNormalizado, nameof(cnpj));
+
+            VerificarQuantidadeCaracteresCnpj(cnpjNormalizado, nameof(cnpj));
+
+            VerificarCaracteresCnpj(cnpjNormalizado, nameof(cnpj));
+
+            VerificarUltimosDoisDigitos(cnpjNormalizado, nameof(cnpj));
 
             Id = Guid.NewGuid();
             TenantId = tenantId;
             RazaoSocial = razaoSocial;
-            Cnpj = cnpj;
+            Cnpj = cnpjNormalizado;
         }
 
         private static void ValidarCampoObrigatorio(string? valor, string campoObrigatorio)
@@ -46,12 +55,74 @@ namespace AssistenciaTecnica.Domain.Entities
             }
         }
 
-        private static void ValidarTentantIdVazio(Guid tenantId, string nomeCampo)
+        private static void ValidarTenantIdVazio(Guid tenantId, string nomeCampo)
         {
             if(tenantId == Guid.Empty)
             {
                 throw new ArgumentException("TentantId está vazio!", nomeCampo);
             }
+        }
+
+        private static string NormalizarCnpj(string cnpj)
+        {
+            cnpj = cnpj.Replace(".", "");
+            cnpj = cnpj.Replace("/", "");
+            cnpj = cnpj.Replace("-", "");
+
+            return cnpj;
+        }
+
+        private static void VerificarQuantidadeCaracteresCnpj(string cnpj, string nomeCampo)
+        {
+            if(cnpj.Length != 14)
+            {
+                throw new ArgumentException("Cnpj Inválido!", nomeCampo);
+            }
+        }
+
+        private static void VerificarCaracteresCnpj(string cnpj, string nomeCampo)
+        {
+            
+
+            foreach(var caractere in cnpj)
+            {
+                bool isDigito = caractere >= '0' && caractere <= '9';
+                bool isLetraMaiuscula = caractere >= 'A' && caractere <= 'Z';
+                bool isLetraMinuscula = caractere >= 'a' && caractere <= 'z';
+
+                if (!isDigito && !isLetraMaiuscula && !isLetraMinuscula)
+                {
+                    throw new ArgumentException("O CNPJ contém caracteres não permitidos.", nomeCampo);
+                }
+
+            }
+        }
+
+        private static void VerificarUltimosDoisDigitos(string cnpj, string nomeCampo)
+        {
+            char penultimo = cnpj[cnpj.Length - 2];
+            char ultimo = cnpj[cnpj.Length - 1];
+            bool penultimoIsDigito = penultimo >= '0' && penultimo <= '9';
+            bool ultimoIsDigito = ultimo >= '0' && ultimo <= '9';
+
+            if(!ultimoIsDigito || !penultimoIsDigito)
+            {
+                throw new ArgumentException("O CNPJ contém caracteres não permitidos.", nomeCampo);
+            }
+        }
+
+        private static int CalcularPrimeiroDigito(string cnpj)
+        {
+            int[] pesos = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma = 0;
+            for(int i = 0; i < pesos.Length; i++)
+            {
+                int valor = cnpj[i] - '0';
+                soma += valor * pesos[i];
+            }
+            int resto = soma % 11;
+            int digito = resto < 2 ? 0 : 11 - resto;
+            return digito;
         }
     }
 }
--- tests/AssistenciaTecnica.Domain.Tests/ClienteTests.cs
+++ tests/AssistenciaTecnica.Domain.Tests/ClienteTests.cs
@@ -78,6 +78,117 @@ public class ClienteTests
         Assert.Equal(tenantId, cliente.TenantId);
     }
 
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void CriarCliente_QuandoCnpjNaoInformado_DeveLancarArgumentException(string? cnpj)
+    {
+        var exception = Assert.Throws<ArgumentException>(() =>
+        {
+            _ = new Cliente(
+                Guid.NewGuid(),
+                "Empresa Exemplo Ltda.",
+                cnpj!
+            );
+        });
+
+        Assert.Equal("cnpj", exception.ParamName);
+    }
+
+    [Fact]
+    public void CriarCliente_QuandoCnpjFormatado_DeveNormalizarCnpj()
+    {
+        string cnpj = "04.252.011/0001-10";
+        string cnpjEsperado = "04252011000110";
+        var cliente = new Cliente(Guid.NewGuid(), "Empresa Exemplo Ltda.", cnpj);
+
+        Assert.Equal(cnpjEsperado, cliente.Cnpj);
+
+    }
+
+    [Fact]
+    public void CriarCliente_QuandoCnpjContemApenasPontuacao_DeveRetornarArgumentException()
+    {
+        var exception = Assert.Throws<ArgumentException>(() =>
+        {
+            _ = new Cliente(
+                Guid.NewGuid(),
+                "Empresa Exemplo Ltda.",
+                "./-"
+            );
+        });
+
+        Assert.Equal("cnpj", exception.ParamName);
+
+    }
+
+    [Theory]
+    [InlineData("123")]
+    [InlineData("123456789012345")]
+    public void CriarCliente_QuandoCnpjTemTamanhoInvalido_DeveLancarArgumentException(string cnpj)
+    {
+        var exception = Assert.Throws<ArgumentException>(() =>
+        {
+            _ = new Cliente(
+                Guid.NewGuid(),
+                "Empresa Exemplo Ltda.",
+                cnpj
+            );
+        });
+
+        Assert.Equal("cnpj", exception.ParamName);
+    }
+
+    [Fact]
+    public void CriarCliente_QuandoCnpjContemCaractereNaoPermitido_DeveRetornarArgumentException()
+    {
+        var exception = Assert.Throws<ArgumentException>(() =>
+        {
+            _ = new Cliente(
+                Guid.NewGuid(),
+                "Empresa Exemplo Ltda.",
+                "12@45678901234"
+            );
+        });
+
+        Assert.Equal("cnpj", exception.ParamName);
+
+    }
+
+    [Theory]
+    [InlineData("AB1234567890A1")] // Letra na penúltima posição
+    [InlineData("AB12345678901A")] // Letra na última posição
+    public void CriarCliente_QuandoDigitosVerificadoresContemLetra_DeveLancarArgumentException(string cnpj)
+    {
+        var exception = Assert.Throws<ArgumentException>(() =>
+        {
+            _ = new Cliente(
+                Guid.NewGuid(),
+                "Empresa Exemplo Ltda.",
+                cnpj
+            );
+        });
+
+        Assert.Equal("cnpj", exception.ParamName);
+
+    }
+
+    [Fact]
+    public void CriarCliente_QuandoDigitoVerificadorIncorreto_DeveLancarArgumentException()
+    {
+        var exception = Assert.Throws<ArgumentException>(() =>
+        {
+            _ = new Cliente(
+                Guid.NewGuid(),
+                "Empresa Exemplo Ltda.",
+                "04252011000111"
+            );
+        });
+
+        Assert.Equal("cnpj", exception.ParamName);
+    }
+
     private static Cliente CriarCliente(Guid tenantId, string razaoSocial = "Empresa Exemplo Ltda.", string cnpj = "12345678901234")
     {
         return new Cliente(tenantId, razaoSocial, cnpj);
